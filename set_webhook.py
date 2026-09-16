e75417b7566c2526252324ed4a150a2c3d3e9f15
Sandaruwan Rajapaksha <thanula2019@gmail.com>
2026-09-17 02:26:41 +0530

    webhook added

---
+++ set_webhook.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+RENDER_URL = "https://dexter-telegram-app.onrender.com"
+
+url = f"https://api.telegram.org/bot{TOKEN}/setWebhook"
+
+response = requests.post(
+    url,
+    data={"url": RENDER_URL},
+    timeout=30
+)
+
+print(response.json())
